3d762925c296d9d05a53a0712bfc4dacb56d04a6
Ihar <iusmanof@gmail.com>
2023-12-08 09:55:13 +0300

    feat: add spaceship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -12,7 +13,11 @@ class AlienInvasion:
         
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
         self.bg_color = self.settings.bg_color
+        self.ship = Ship(self)
+
+
 
     def run_game(self):
         while True:
@@ -21,6 +26,7 @@ class AlienInvasion:
                         sys.exit()
                 
                 self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
 
                 pygame.display.flip()
 
