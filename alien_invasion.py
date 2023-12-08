b89fb1829ce6ca459c49b95fde6cd5476d0456c0
Ihar <iusmanof@gmail.com>
2023-12-08 16:06:48 +0300

    feat: bullet and limit

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
 
@@ -11,17 +12,27 @@ class AlienInvasion:
 
         pygame.init()
         
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.bg_color = self.settings.bg_color
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
-            self._update_events()
+            self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                print(len(self.bullets))
+
+            self._update_screen()
         
             pygame.display.flip()
 
@@ -43,6 +54,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):    
         if event.key == pygame.K_RIGHT:
@@ -50,9 +63,17 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
-    def _update_events(self):
+    def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+    
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet  = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
 if __name__ == "__main__":
     ai = AlienInvasion()
--- settings.py
+++ settings.py
@@ -5,4 +5,11 @@ class Settings():
         self.bg_color = (115,215,255)
         self.screen_width = 1200
         self.screen_height = 800
+        
         self.ship_speed = 1.5
+
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (156, 138, 86)
+        self.bullet_allowed = 3
